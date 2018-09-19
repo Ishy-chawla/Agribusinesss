c692888c9fff946a1d0cf91c74d015e3f3e67461
IshmeenChawla <ishychawla@gmail.com>
2018-09-19 17:46:42 +1000

    Test commit

---
+++ views/try.js
@@ -0,0 +1,23 @@
+//function graph() {
+  //  document.write("working!!!")};
+
+function graph() {
+    var chosen = document.getElementById('try');
+    document.write(chosen.value);
+    console.log(chosen.value);
+    if(chosen.value==="1")
+      document.write("graph 1 working!!!");
+    else
+      document.write("graph 2 working!!!")};
+
+   /* var chosen = document.getElementById('try');
+    document.write(chosen.value);
+    console.log(chosen.value)};
+    //if(chosen==="a")
+      //document.write("graph 1 working!!!");
+    //else
+      //document.write("graph 2 working!!!")};
+    function graph2() {
+    var chosen = document.getElementById('try');
+    document.write(chosen.value);
+    console.log(chosen.value)};*/
